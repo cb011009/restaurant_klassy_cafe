e4b09a8751250d03c993b61b57c09dfd38bff75b
Dulanmi Himansa De Silva <cb011009@students.apiit.lk>
2023-09-07 11:41:09 +0530

    service information

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -278,7 +278,7 @@
                         <div id="contact">
                             <div class="row">
                               <div class="col-lg-12">
-                                <h4>Restaurant Facilities</h4>
+                                <h4>Service Information</h4>
                             </div>
                                 <div id=contact-row-container>
                                    <div id=contact-row>
@@ -287,21 +287,21 @@
                                    </div>
                                    <div id=contact-row>
                                      <p>Alchohol</p>
-                                     <img src="assets/images/thai_Cuisine.jpg" >
+                                     <img src="assets/images/alchohol.jpg" >
                                    </div>
                                    <div id=contact-row>
-                                    <p>Wifi</p>
-                                    <img src="assets/images/thai_Cuisine.jpg" >
+                                    <p>Pets</p>
+                                    <img src="assets/images/no_pets.jpg" >
                                   </div>
                                   <div id=contact-row>
-                                    <p>Alchohol</p>
-                                    <img src="assets/images/thai_Cuisine.jpg" >
+                                    <p>Parking</p>
+                                    <img src="assets/images/vallet.jpg" >
                                   </div>
                               </div>
                               <div id=contact-row-container>
                                 <div id=contact-row>
                                   <p>Wifi</p>
-                                  <img src="assets/images/thai_Cuisine.jpg" >
+                                  <img src="assets/images/visa.jpg" >
                                 </div>
                                 <div id=contact-row>
                                   <p>Alchohol</p>
@@ -318,77 +318,7 @@
                            </div>
 
                         </div>
-                        <!--<form id="contact" action="" method="post">
-                          <div class="row">
-                            <div class="col-lg-12">
-                                <h4>Restaurant Facilities</h4>
-                            </div>
-                            <div class="col-lg-6 col-sm-12">
-                              <fieldset>
-                                <input name="name" type="text" id="name" placeholder="Your Name*" required="">
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-6 col-sm-12">
-                              <fieldset>
-                              <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email Address" required="">
-                            </fieldset>
-                            </div>
-                            <div class="col-lg-6 col-sm-12">
-                              <fieldset>
-                                <input name="phone" type="text" id="phone" placeholder="Phone Number*" required="">
-                              </fieldset>
-                            </div>
-                            <div class="col-md-6 col-sm-12">
-                              <fieldset>
-                                <select value="number-guests" name="number-guests" id="number-guests">
-                                    <option value="number-guests">Number Of Guests</option>
-                                    <option name="1" id="1">1</option>
-                                    <option name="2" id="2">2</option>
-                                    <option name="3" id="3">3</option>
-                                    <option name="4" id="4">4</option>
-                                    <option name="5" id="5">5</option>
-                                    <option name="6" id="6">6</option>
-                                    <option name="7" id="7">7</option>
-                                    <option name="8" id="8">8</option>
-                                    <option name="9" id="9">9</option>
-                                    <option name="10" id="10">10</option>
-                                    <option name="11" id="11">11</option>
-                                    <option name="12" id="12">12</option>
-                                </select>
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-6">
-                                <div id="filterDate2">    
-                                  <div class="input-group date" data-date-format="dd/mm/yyyy">
-                                    <input  name="date" id="date" type="text" class="form-control" placeholder="dd/mm/yyyy">
-                                    <div class="input-group-addon" >
-                                      <span class="glyphicon glyphicon-th"></span>
-                                    </div>
-                                  </div>
-                                </div>   
-                            </div>
-                            <div class="col-md-6 col-sm-12">
-                              <fieldset>
-                                <select value="time" name="time" id="time">
-                                    <option value="time">Time</option>
-                                    <option name="Breakfast" id="Breakfast">Breakfast</option>
-                                    <option name="Lunch" id="Lunch">Lunch</option>
-                                    <option name="Dinner" id="Dinner">Dinner</option>
-                                </select>
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-12">
-                              <fieldset>
-                                <textarea name="message" rows="6" id="message" placeholder="Message" required=""></textarea>
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-12">
-                              <fieldset>
-                                <button type="submit" id="form-submit" class="main-button-icon">Make A Reservation</button>
-                              </fieldset>
-                            </div>
-                          </div>
-                        </form>-->
+                       
         
         </div>
     </div>
