dd0cb9ffb12bb0cc02fdb72f20a56c6aad06a92b
Dulanmi Himansa De Silva <cb011009@students.apiit.lk>
2023-09-07 11:46:31 +0530

    font awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Klassy Cafe - Restaurant HTML Template</title>
 <!--
     
