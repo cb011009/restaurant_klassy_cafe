d2563c28804758a61412130ef6ff340bed29f7f3
Dulanmi Himansa De Silva <cb011009@students.apiit.lk>
2023-09-08 08:16:34 +0530

    registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed','unique:users'],
             'date_of_birth' => ['required', 'date','adult'],
-            'phone_number' => ['required', 'string', 'max:15'],
+            'phone_number' => ['required', 'string', 'max:10'],
             'address' => ['required', 'string', 'max:255'],
         ]); 
     }
